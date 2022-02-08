3a7433bbfa6e7fbd560ab31f2a63037d03d38b47
Ümit Türkmen <umit.482@gmail.com>
2022-02-08 15:14:27 +0300

    trycath ödev

--- Operators.cs
+++ Operators.cs
@@ -1,59 +1,60 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace degiskenler
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Atama ve İşlemli Operatorlar
-            int x = 3;
-            int y = 5;
-            y = y+2;
-            //+-/*
-            //Mantıksal O.
-            // || && !
-            
-            bool isSUccess = true;
-            bool isCompleted = false;
-            if(isSUccess && isCompleted)
-                Console.WriteLine("Perfect");
-            if(isSUccess || isCompleted)
-                Console.WriteLine("Great");
-            if(isSUccess && isCompleted)
-                Console.WriteLine("Fine");
-
-
-            //İlişkisel Operatörler
-            // <> >= <= == !=
-
-            int a = 3;
-            int b = 4;
-            bool sonuc = a<b;
-            //vs...
-            //Aritmetik Operatörler
-            // / * + -
-
-            int sayi = 10;
-            int sayi1 = 5;
-            int sonuc2 = sayi/sayi1;
-            sonuc2 = sayi1 ++;
-
-            // % : Mod alır
-            int sonuc3 = 20%3; //3e bölümünden kalanı bulur.
-            
-
-
-
-            
-        }
-        
-    }
-}
-
+﻿//using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Threading;
+//using System.Threading.Tasks;
+//
+//namespace degiskenler2
+//{
+//    class Operators
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Atama ve İşlemli Operatorlar
+//            int x = 3;
+//            int y = 5;
+//            y = y+2;
+//            //+-/*
+//            //Mantıksal O.
+//            // || && !
+//            
+//            bool isSUccess = true;
+//            bool isCompleted = false;
+//            if(isSUccess && isCompleted)
+//                Console.WriteLine("Perfect");
+//            if(isSUccess || isCompleted)
+//                Console.WriteLine("Great");
+//            if(isSUccess && isCompleted)
+//                Console.WriteLine("Fine");
+//
+//
+//            //İlişkisel Operatörler
+//            // <> >= <= == !=
+//
+//            int a = 3;
+//            int b = 4;
+//            bool sonuc = a<b;
+//            //vs...
+//            //Aritmetik Operatörler
+//            // / * + -
+//
+//            int sayi = 10;
+//            int sayi1 = 5;
+//            int sonuc2 = sayi/sayi1;
+//            sonuc2 = sayi1 ++;
+//
+//            // % : Mod alır
+//            int sonuc3 = 20%3; //3e bölümünden kalanı bulur.
+//            
+//
+//
+//
+//            
+//        }
+//        
+//    }
+//}
+//
+//
